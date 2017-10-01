e5aebfcfc58b82eda779360c6bfe79a14890bdf7
johndacost <john.dacost@gmail.com>
2017-10-01 11:46:51 +0000

    Apply fixes from StyleCI

--- app/Cart.php
+++ app/Cart.php
@@ -5,23 +5,25 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Cart model
+ * Cart model.
  */
 class Cart extends Model
 {
-	/**
-	 * A cart belong to an user
-	 * @return an user object
-	 */
-    public function user(){
-    	return $this->belongsTo(User::class);
+    /**
+     * A cart belong to an user.
+     * @return an user object
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
     /**
-     * Any cart may have many product
+     * Any cart may have many product.
      * @return an array of products object
      */
-    public function products(){
-    	return $this->belongsToMany(Product::class);
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
     }
 }
