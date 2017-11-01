ee47fcdb27093ba9629e5e49a882d62d445cfeae
73VW <mael.pedretti@hotmail.fr>
2017-11-01 15:06:25 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     public function index()
     {
-    	$products = \App\Product::all();
+        $products = \App\Product::all();
 
         return view('home.index', compact('products'));
     }
