1e8692b870d23ce6165ad4b1d53b9a1d28c3289d
Pedro Costa <jules007.cesar@hotmail.com>
2017-12-12 15:27:13 +0100

    Style udpate
    Added owl carousel
    Added search bar
    Added pagination
    Added grid/list view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = \App\Product::all();
+        $products = \App\Product::where("user_id","<>",auth()->user()->id)->orderBy("created_at", "desc")->paginate(6);
+
+        // if ($request->ajax()) {
+        //     return view('home.index', compact('products'));
+        // }
 
         return view('home.index', compact('products'));
     }
