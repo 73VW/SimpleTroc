23113b9ab1ece29b665376bd1f4c24b00ccdc79c
Mael Pedretti <mael.pedretti@hotmail.fr>
2017-12-13 12:20:06 +0100

    Implemented troc mechanism
    Added create and store routes
    Added create and store methods in BarterController
    Added views for barter creation
    Updated home view to redirect to the right place

---
+++ resources/views/barter/create.blade.php
@@ -0,0 +1,71 @@
+<div class="card" style="margin-bottom:10px; background-color: #563d7c; color:white">
+  <div class="body"><h1 class="display-4 text-center">Produit voulu</h1></div>
+</div>
+<div class="card">
+  <div class="card-body">
+      <table class="table table-hover table-responsive">
+        <caption>Produit voulu</caption>
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">name</th>
+            <th scope="col">number of image</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr id="product_{{$wanted->id}}">
+            @if (count($wanted->pictures()->get()))
+            <td class="align-middle">
+              <img src="{{$wanted->pictures()->first()->link()}}" alt="" class="img-fluid" width="200px" height="200px">
+            </td>
+            @else
+              <td class="align-middle">no image</td>
+            @endif
+            <td class="align-middle">{{$wanted->name}}</td>
+            <td class="align-middle">{{count($wanted->pictures()->get())}}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div><!-- END CARD-BODY -->
+</div><!--END CARD -->
+<div class="card" style="margin-bottom:10px; background-color: #563d7c; color:white">
+  <div class="body"><h1 class="display-4 text-center">List of your products</h1></div>
+</div>
+<div class="card">
+  <div class="card-body">
+    <table class="table table-hover table-responsive">
+      <caption>Which product do you want to troc?</caption>
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">name</th>
+          <th scope="col">number of image</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+      	@foreach ($products as $product)
+    	<tr id="product_{{$product->id}}">
+          @if (count($product->pictures()->get()))
+          <td class="align-middle">
+            <img src="{{$product->pictures()->first()->link()}}" alt="" class="img-fluid" width="200px" height="200px">
+          </td>
+          @else
+            <td class="align-middle">no image</td>
+          @endif
+          <td class="align-middle">{{$product->name}}</td>
+          <td class="align-middle">{{count($product->pictures()->get())}}</td>
+          <td class="align-middle center">
+            <form action="/profile/barter/store" method="POST">
+                {{ csrf_field() }}
+                <input type="hidden" name="my_product_id" value="{{$product->id}}"/>
+                <input type="hidden" name="product_id" value="{{$wanted->id}}"><br>
+              <input class="btn btn-outline-dark" type="submit" aria-hidden="true" value="Troc">
+            </form>
+          </td>
+        </tr>
+      	@endforeach
+      </tbody>
+    </table>
+  </div><!-- END CARD-BODY -->
+</div><!--END CARD -->
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,74 +1,80 @@
 @extends('layouts.app')
 
+
 @section('content')
-<?php 
+<?php
 Carbon\Carbon::setLocale("fr");
 $i = 0;
 ?>
 <div class="container-fluid" id="main">
-	<div class="row" style="padding-left: 3%;">
-		<div class="col-md-8" id="search_bar" >
-			<div class="input-group col-md-12">
-				<input type="text" class="form-control" placeholder="Recherche" aria-describedby="basic-addon2">
-				<span class="input-group-btn">
-                	<button type="button" class="btn btn-inverse" id="basic-addon2">Chercher</button>
+    @include('layouts.sessions-flash')
+    <div class="row" style="padding-left: 3%;">
+        <div class="col-md-8" id="search_bar" >
+            <div class="input-group col-md-12">
+                <input type="text" class="form-control" placeholder="Recherche" aria-describedby="basic-addon2">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-inverse" id="basic-addon2">Chercher</button>
                 </span>
-			</div>
-		</div>
-		<div class="col-md-2" style="padding-top: 2px;">
-			<div class="col-md-12 bouton">
-				<bouton id="grid" class="btn btn-outline-secondary active fa fa-th col-md-4" aria-hidden="true" href="#"></bouton>
-				<bouton id="list" class="btn btn-outline-secondary fa fa-list col-md-4" aria-hidden="true" href="#"></bouton>
-			</div>	
-		</div>
-	</div>
-	<div id="products" class="list-unstyled row">
-	    @foreach($products as $product)
-	    <div class="card item element">
-		    <div class="thumbnail">
-		        <div class="owl-carousel owl-theme">
-		        	@foreach($product->pictures()->get() as $picture)
-		       			<div class="owl-item">
-		   					<div class="item">
-		        				<img class="image card-img-top" src="{{$picture->link()}}" alt="Card image cap" width="350" height="200">
-		        			</div>
-		        		</div>
-		       		@endforeach
-		        </div>
-		        <div class="card-body">
-		            <div class="row head">
-		            	<h4 class="card-title col-sm-8" id="pname{{$i}}">{{$product->name}}</h4>
-		            	<button class="btn btn-outline-secondary fa fa-user-circle-o col-sm-3" id="ubtn{{$i}}" type="button" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{$product->user()->first()->name}}" onmouseover="hoverButton(this);"> </button>
-		            </div>
-		            <div class="row description" id="desc">
-		            	<p class="card-text">{{$product->description}}</p>
-		           	</div>
-			        <div class="row details">
-			            <div class="col-md-7 card-text price" id="pPrice{{$i}}">
-			               <p class="lead">{{$product->price}} frs</p>
-		                </div>
-			            <div class="col-md-5 bouton" id="btn{{$i++}}">
-			            	<a class="btn btn-primary fa fa-exchange" aria-hidden="true" href="#"></a>
-			                <a class="btn btn-success fa fa-money" aria-hidden="true" href="#"></a>
-			            </div>
-			        </div>
-		        </div>
-		        <div class="card-footer text-muted">
-		        	<p class="row card-text date">{{$product->created_at->diffForHumans()}}</p>
-		       	</div>
-		    </div> 
-	    </div>
-        @endforeach	
-        <div class="row paginationPerso">
-	    	{{$products->links()}}
-	    </div>
+            </div>
+        </div>
+        <div class="col-md-2" style="padding-top: 2px;">
+            <div class="col-md-12 bouton">
+                <bouton id="grid" class="btn btn-outline-secondary active fa fa-th col-md-4" aria-hidden="true" href="#"></bouton>
+                <bouton id="list" class="btn btn-outline-secondary fa fa-list col-md-4" aria-hidden="true" href="#"></bouton>
+            </div>
+        </div>
     </div>
-	<div id="categories" style="background-color: #22427C;">
-    	<ul class="navbar-nav" id="categories_list">		
-	    	@foreach(\App\Category::all() as $category)
-				<a class="nav-link" href="#">{{$category->name}}</a>
-			@endforeach
-		</ul>
+    <div id="products" class="list-unstyled row">
+        @foreach($products as $product)
+        <div class="card item element">
+            <div class="thumbnail">
+                <div class="owl-carousel owl-theme">
+                    @foreach($product->pictures()->get() as $picture)
+                    <div class="owl-item">
+                        <div class="item">
+                            <img class="image card-img-top" src="{{$picture->link()}}" alt="Card image cap" width="350" height="200">
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+                <div class="card-body">
+                    <div class="row head">
+                        <h4 class="card-title col-sm-8" id="pname{{$i}}">{{$product->name}}</h4>
+                        <button class="btn btn-outline-secondary fa fa-user-circle-o col-sm-3" id="ubtn{{$i}}" type="button" aria-hidden="true" data-toggle="tooltip" data-placement="right" title="{{$product->user()->first()->name}}" onmouseover="hoverButton(this);"> </button>
+                    </div>
+                    <div class="row description" id="desc">
+                        <p class="card-text">{{$product->description}}</p>
+                    </div>
+                    <div class="row details">
+                        <div class="col-md-7 card-text price" id="pPrice{{$i}}">
+                            <p class="lead">{{$product->price}} frs</p>
+                        </div>
+                        <div class="col-md-5 bouton" id="btn{{$i}}">
+                            <form action="profile/barter/create" method="POST">
+                                {{ csrf_field() }}
+                                <input type="hidden" name="product_id" value="{{$product->id}}"><br>
+                                <input class="btn btn-primary fa fa-exchange" type="submit" aria-hidden="true" value="submit">
+                            </form>
+                            <a  class="btn btn-success fa fa-money" aria-hidden="true" href="#"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-muted">
+                    <p class="row card-text date">{{$product->created_at->diffForHumans()}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        <div class="row paginationPerso">
+            {{$products->links()}}
+        </div>
+    </div>
+    <div id="categories" style="background-color: #22427C;">
+        <ul class="navbar-nav" id="categories_list">
+            @foreach(\App\Category::all() as $category)
+            <a class="nav-link" href="#">{{$category->name}}</a>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endsection
